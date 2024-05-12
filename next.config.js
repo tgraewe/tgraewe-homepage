b17d60c3ca9ef565e32fd00452c03c88370d9923
Saebelfechter <61062363+tgraewe@users.noreply.github.com>
2024-05-12 15:38:48 -0400

    removed hydration overlay

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-const {
-  withHydrationOverlay,
-} = require("@builder.io/react-hydration-overlay/next");
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
 module.exports = withHydrationOverlay({
   reactStrictMode: true
-})(nextConfig);
+}
