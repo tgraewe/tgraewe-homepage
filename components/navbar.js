72129723abc701dbd95fb7cdc310b70d5d3ab161
Saebelfechter <61062363+tgraewe@users.noreply.github.com>
2024-05-14 00:24:06 -0400

    Update navbar.js
    Mainly fixed alignment and center issues
    
    Added a source code button

--- components/navbar.js
+++ components/navbar.js
@@ -1,24 +1,25 @@
+import { forwardRef } from 'react'
 import Logo from './logo'
 import NextLink from 'next/link'
-import{
-    Container,
-    Box,
-    Link,
-    Stack,
-    Heading,
-    Flex,
-    Menu,
-    MenuItem,
-    MenuList,
-    MenuButton,
-    IconButton,
-    useColorModeValue
+import {
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValue
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
-import ThemedToggleButton from '../components/theme-toggle-button'
+import ThemeToggleButton from './theme-toggle-button'
+import { IoLogoGithub } from 'react-icons/io5'
 
-
-const LinkItem = ({ href, path, target, children}) => {
+const LinkItem = ({ href, path, target, children, ...props }) => {
   const active = path === href
   const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
   return (
@@ -30,86 +31,105 @@ const LinkItem = ({ href, path, target, children}) => {
       bg={active ? 'grassTeal' : undefined}
       color={active ? '#202023' : inactiveColor}
       target={target}
-    >
+      {...props}>
       {children}
     </Link>
   )
 }
 
+const MenuLink = forwardRef((props, ref) => (
+  <Link ref={ref} as={NextLink} {...props} />
+))
+
 const Navbar = props => {
-    const { path } = props
-  
-    return (
-      <Box
-        position="fixed"
-        as="nav"
-        w="100%"
-        bg={useColorModeValue('#ffffff40', '#20202380')}
-        style={{ backdropFilter: 'blur(10px)' }}
-        zIndex={1}
-        {...props}
-      >
-        <Container 
-        display="flex" 
-        p={2} 
-        maxW="container,md"
+  const { path } = props
+
+  return (
+    <Box
+      position="fixed"
+      as="nav"
+      w="100%"
+      bg={useColorModeValue('#ffffff40', '#20202380')}
+      css={{ backdropFilter: 'blur(10px)' }}
+      zIndex={2}
+      {...props}>
+      <Container
+        display="flex"
+        p={2}
+        maxW="container.md"
         wrap="wrap"
         align="center"
-        justify="space-between"
-        >
-          <Flex align="center" mr={5}>
-            <Heading as="h1" size="lg" letterSpacing={'tighter'}>
-              <Logo />
-            </Heading>
-          </Flex>
-          <Stack
+        justify="space-between">
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+            <Logo />
+          </Heading>
+        </Flex>
+
+        <Stack
           direction={{ base: 'column', md: 'row' }}
           display={{ base: 'none', md: 'flex' }}
           width={{ base: 'full', md: 'auto' }}
           alignItems="center"
           flexGrow={1}
-          mt={{ base: 4, md: 0 }}
-        >
-            <LinkItem href="/works" path={path}>
+          mt={{ base: 4, md: 0 }}>
+          <LinkItem href="/works" path={path}>
             Works
-            </LinkItem>
-            <LinkItem href="/blog" path={path}>
+          </LinkItem>
+          <LinkItem href="/blog" path={path}>
             Blog
-            </LinkItem>
-            <LinkItem href="/toolkit" path={path}>
+          </LinkItem>
+          <LinkItem href="/toolkit" path={path}>
             Toolkit
-            </LinkItem>
-          </Stack>
+          </LinkItem>
+          <LinkItem
+            target="_blank"
+            href="https://github.com/tgraewe/tgraewe-homepage"
+            path={path}
+            display="inline-flex"
+            alignItems="center"
+            style={{ gap: 4 }}
+            pl={2}>
+            <IoLogoGithub />
+            Source
+          </LinkItem>
+        </Stack>
 
-          <Box flex={1} align="right">
-          <ThemedToggleButton/>
-            <Box ml={2} display={{base: 'inline-block', md:'none'}}>
-              <Menu isLazy id="navbar-menu">
-                <MenuButton
-                  as={IconButton}
-                  icon={<HamburgerIcon />}
-                  variant="outline"
-                  aria-label="Options"
-                />
-                <MenuList>
-                  <NextLink href="/works" passHref>
-                    <MenuItem as={Link}>Works</MenuItem>
-                  </NextLink>
-                  <NextLink href="/blog" passHref>
-                    <MenuItem as={Link}>Blog</MenuItem>
-                  </NextLink>
-                  <NextLink href="/toolkit" passHref>
-                    <MenuItem as={Link}>Toolkit</MenuItem>
-                  </NextLink>
-                </MenuList>
-              </Menu>
-            </Box>
+        <Box flex={1} align="right">
+          <ThemeToggleButton />
+
+          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <Menu isLazy id="navbar-menu">
+              <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                variant="outline"
+                aria-label="Options"/>
+              <MenuList>
+                <MenuItem as={MenuLink} href="/">
+                  About
+                </MenuItem>
+                <MenuItem as={MenuLink} href="/works">
+                  Works
+                </MenuItem>
+                <MenuItem as={MenuLink} href="/blog">
+                  Blog
+                </MenuItem>
+                <MenuItem as={MenuLink} href="/toolkit">
+                  Toolkit
+                </MenuItem>
+                <MenuItem
+                  as={Link}
+                  href="https://github.com/tgraewe/tgraewe-homepage">
+                  Source Code
+                </MenuItem>
+              </MenuList>
+            </Menu>
           </Box>
+        </Box>
+      </Container>
+    </Box>
+  )
+}
 
-
-        </Container>
-      </Box>
-    )
-  }
-  
-  export default Navbar;
+export default Navbar
