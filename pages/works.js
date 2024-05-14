6679d6b527a9184bac67b5fb363fc6b97ab4eb10
Saebelfechter <61062363+tgraewe@users.noreply.github.com>
2024-05-14 13:23:32 -0400

    grammar fixes

--- pages/works.js
+++ pages/works.js
@@ -20,15 +20,15 @@ const Works = () => {
                     id="inspireqr" 
                     title="Inspire-QR" 
                     thumbnail={inspireqr}>
-                    An award winning website project for mental health awareness on college campuses
+                    An award-winning website project for mental health awareness on college campuses.
                     </WorkGridItem>
                 </Section>
                 <Section delay={0.4}>
                     <WorkGridItem 
                     id="ehp" 
-                    title="Ethical Hacker's Website" 
+                    title="Ethical Hacking website" 
                     thumbnail={ehp}>
-                    Ethical Hackers of Purdue website
+                    Ethical Hackers of Purdue website.
                     </WorkGridItem>
                 </Section>
                 <Section delay={0.4}>
@@ -36,7 +36,7 @@ const Works = () => {
                     id="secondbrain" 
                     title="Second Brain" 
                     thumbnail={secondbrain}>
-                    A second brain note taking repoistory
+                    A second brain note-taking repository.
                     </WorkGridItem>
                 </Section>
                 <Section delay={0.4}>
@@ -44,7 +44,7 @@ const Works = () => {
                     id="raspi" 
                     title="Home DNS" 
                     thumbnail={raspi}>
-                    Raspi home DNS/VPN server
+                    Raspi home DNS/VPN server.
                     </WorkGridItem>
                 </Section>
             </SimpleGrid>
@@ -55,7 +55,7 @@ const Works = () => {
             </Heading>
             <SimpleGrid columns={[1,1,2]} gap={6}>
                 <Section delay={0.4}>
-                <p>First collab incoming. Keep an eye out ;)</p>
+                <p>First collab is incoming. Keep an eye out ;)</p>
                 </Section>
             </SimpleGrid>
             <Divider my={6}/>
@@ -69,7 +69,7 @@ const Works = () => {
                     id="oldsite" 
                     title="Original Porfolio" 
                     thumbnail={oldsite}>
-                    An original website made with no fameworks
+                    An original website made with no frameworks.
                     </WorkGridItem>
                 </Section>
                 <Section delay={0.4}>
@@ -77,15 +77,15 @@ const Works = () => {
                     id="minecraft" 
                     title="Game Server" 
                     thumbnail={minecraft}>
-                    A dynamic minecraft server
+                    A vanilla, tweaked Minecraft game server.
                     </WorkGridItem>
                 </Section>
                 <Section delay={0.4}>
                     <WorkGridItem 
                     id="computer" 
-                    title="Computer" 
+                    title="Custom Computer" 
                     thumbnail={computer}>
-                    A custom computer and workstation for a homelab
+                    A custom built gaming PC and workstation for a home lab.
                     </WorkGridItem>
                 </Section>
             </SimpleGrid>
