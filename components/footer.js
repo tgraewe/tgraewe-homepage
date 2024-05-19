c60dc6a994a7434dec1e59ec4583f6847eb9db06
Saebelfechter <61062363+tgraewe@users.noreply.github.com>
2024-05-18 23:33:09 -0400

    updated href

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} <a rel="nofollow" href="index.html">Tommaso Graewe</a>. All Rights Reserved.
+      &copy; {new Date().getFullYear()} <a rel="nofollow" href="">Tommaso Graewe</a>. All Rights Reserved.
     </Box>
   )
 }
