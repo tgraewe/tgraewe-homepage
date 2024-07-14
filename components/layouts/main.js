11ec6a63937eaca7becdd4097235988413f6a68f
tgraewe <61062363+tgraewe@users.noreply.github.com>
2024-07-14 14:24:11 -0400

    favicons

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,6 +8,10 @@ const Main = ({ children, router }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Tommaso's - Homepage</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png"></link>
+        <link rel="manifest" href="images/site.webmanifest"></link>
       </Head>
       
       <Navbar></Navbar>
