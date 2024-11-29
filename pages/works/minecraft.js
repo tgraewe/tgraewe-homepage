a6e79ec0c1d057997488c55ed644efd2da6a9c90
tgraewe <61062363+tgraewe@users.noreply.github.com>
2024-11-29 12:29:23 -0500

    Update minecraft.js

--- pages/works/minecraft.js
+++ pages/works/minecraft.js
@@ -16,22 +16,20 @@ const Work = () => {
         <Layout title="Game Server">
             <Container>
                 <Title>
-                    Game Server <Badge>2022 - 2023</Badge>
+                    t0matoes.playit.gg <Badge>2022 - 2023</Badge>
                 </Title>
                 <P>
-                A Minecraft server hosted on my home infrastructure.
-                Supports quality-of-life features for players including a website with a live world map and more achievements to complete.
-                Added layers of security to the network and system for the users and infrastructure, 
-                to avoid port scans from overseas or unwelcomed clients.
+                A Minecraft server hosted on my home VM cluster. 
+                Uses secure VPN tunnel for clients and global anycast network to avoid portscans.
                 </P>
                 <List ml={4} my={4}>
                     <ListItem>
                         <Meta>Platform</Meta>
-                        <span>Windows/Minecraft Java Edition</span>
+                        <span>Minecraft Java Edition</span>
                     </ListItem>
                     <ListItem>
                         <Meta>Stack</Meta>
-                        <span>Java Development Kit (JDK)/HP ProLiant hardware/RAID hardware/Ubuntu Server/Cloudflare Domain Name System (DNS)</span>
+                        <span>Java Development Kit (JDK) / Ubuntu Server / Cloudflare DNS</span>
                     </ListItem>
                     <WorkImage src="/images/projects/minecraft.png" alt="about"></WorkImage>                   
                     <WorkImage src="/images/projects/minecraft_01.png" alt="resume"></WorkImage>                   
