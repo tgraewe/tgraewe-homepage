1361564857eccddce4a09bf0c9523219bb7ccaa8
tgraewe <61062363+tgraewe@users.noreply.github.com>
2024-12-09 17:52:27 -0500

    inital homelab

--- pages/works/homelab.js
+++ pages/works/homelab.js
@@ -19,18 +19,27 @@ const Work = () => {
                     Homelab <Badge>2024 - Present</Badge>
                 </Title>
                 <P>
-                brew brew brew
+                My home network setup features multiple segments built around a 5-port Gigabit Layer 2 switch at its heart. I've divided the network into two main parts: a wireless LAN running on 192.168.1.0/24 and a private LAN on 10.0.0.0/24. For internet access, I use a modem/wireless router that links to my ISP. To ensure solid WiFi coverage throughout my space, I implemented a bridged point-to-point setup using two routers - an OpenWRT router (ap1-hoosier.home) and a TP-Link router (ap2-hoosier.home).
+                </P>
+                <P>
+                I've connected several essential systems to my Layer 2 switch, including my Windows 10 management workstation (mgmt.hoosier.home), a Proxmox virtualization cluster (cluster.hoosier.home), a local nameserver (ns.hoosier.home). Within my Proxmox cluster, I run an Ubuntu Server (mc.hoosier.home) as a virtual machine, allowing me to efficiently manage and deploy services as needed.
+                </P>
+                <P>This homelab has been invaluable for hands-on learning experience, giving me a environment to experiment with various networking concepts and tools. The lab has repeatedly proven itself as an excellent platform for troubleshooting and problem-solving, helping me develop practical skills that directly translate to professional IT scenarios.
                 </P>
                 <List ml={4} my={4}>
                     <ListItem>
-                        <Meta>Location</Meta>
-                        <span>Home</span>
+                        <Meta>Experience</Meta>
+                        <span>Network Administration/Architecture</span>
                     </ListItem>
                     <ListItem>
                         <Meta>Equipment</Meta>
-                        <span>HP ProLiant G7 / Proxmox Virtual Environment / Raspberry Pi / TP-Link Wireless Routers / Network Cabling</span>
+                        <span>HP ProLiant G7 / Raspberry Pi / TP-Link Wireless Routers / TP-Link 5 Port Switch / Network Cabling</span>
                     </ListItem>
-                    <WorkImage src="/images/projects/minecraft.png" alt="about"></WorkImage>                                    
+                    <ListItem>
+                        <Meta>Software</Meta>
+                        <span>Proxmox Virtual Environment / Ubuntu Server / Raspberry Pi OS Lite / OpenWRT / TP-Link Firmware</span>
+                    </ListItem>
+                    <WorkImage src="/images/projects/homelab/diagram.png" alt="about"></WorkImage>                                    
                 </List>
             </Container>
         </Layout>
