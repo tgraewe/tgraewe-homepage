5d823bf55e36975dcb3343d09183dfee1aa00797
tom🗻 <61062363+tgraewe@users.noreply.github.com>
2026-04-04 21:02:05 -0400

    Update theme.js

--- libs/theme.js
+++ libs/theme.js
@@ -25,7 +25,7 @@ const components = {
   },
   Link: {
     baseStyle: props => ({
-      color: mode('blue', 'orange')(props),
+      color: mode('navy', 'purple')(props),
       textUnderlineOffset: 3
     })
   }
